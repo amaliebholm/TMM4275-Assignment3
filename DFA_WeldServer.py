290f75f29c2fdbaf521f5286eb917f48e1f32729
Amalie Berge Holm <eaholm@stud.ntnu.no>
2021-04-19 15:43:18 +0200

    uploading file

--- DFA_WeldServer.py
+++ DFA_WeldServer.py
@@ -1,5 +1,7 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
+import socketserver
+import io
+import cgi
 
 # Set inital parameters
 nozzle = 0 
@@ -9,7 +11,7 @@ HOST_NAME = '127.0.0.1'  # locathost - http://127.0.0.1
 PORT_NUMBER = 1234
 
 # Handler of HTTP requests / responses
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(SimpleHTTPRequestHandler):
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
@@ -36,14 +38,14 @@ class MyHandler(BaseHTTPRequestHandler):
                 bytes("<body><p>Current path: " + path + "</p>", "utf-8"))
             s.wfile.write(bytes("<body><p>Let's check the volumes for the welding gun!</p>", "utf-8"))
             s.wfile.write(bytes('</body></html>', "utf-8"))
-        elif path.find("/fileUploadScript.php") != -1:
+        elif path.find("/uploadFile") != -1:
             s.send_response(200)
             s.send_header("Content-type", "text/html")
             s.end_headers()
-            s.wfile.write(bytes('<form action="fileUploadScript.php" method="post" enctype="multipart/form-data">', 'utf-8'))
+            s.wfile.write(bytes('<form action="uploadFile" method="post" enctype="multipart/form-data">', 'utf-8'))
             s.wfile.write(bytes('<html><body><h2>Weldability Checker:</h2>', "utf-8"))
 
-            s.wfile.write(bytes('<br>Upload your .prt file containing the welding lines<br><input type="file" name="the_file" id="fileToUpload">', "utf-8"))
+            s.wfile.write(bytes('<br>Upload your .prt file containing the welding lines<br><input type="file" id="myFile" name="myFile">', "utf-8"))
             s.wfile.write(bytes('<br><input type="submit" name="Upload file" value="Upload file">', "utf-8"))
 
             s.wfile.write(bytes('<br><br>Selct type of nozzle:<br><select name="nozzle" id="nozzle"><option value="RECESSED">Recessed</option><option value="FLUSH">Flush</option><option value="PROTRUDING">Protruding</option><option value="ADJUSTABLE">Adjustable</option><option value="CUSTOM">Custom</option></select>', "utf-8"))
@@ -61,29 +63,38 @@ class MyHandler(BaseHTTPRequestHandler):
                 bytes("<body><p>The path: " + path + "</p>", "utf-8"))
             s.wfile.write(bytes('</body></html>', "utf-8"))
 
-def do_POST(s):
-
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
+    def do_POST(s):
+        r, info = self.deal_post_data()
+        print(r, info, "by: ", self.client_address)
+        f = io.BytesIO()
+        if r:
+            f.write(b"Success\n")
+        else:
+            f.write(b"Failed\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        if f:
+            self.copyfile(f, self.wfile)
+            f.close()      
 
         # Check what is the path
         path = s.path
         print("Path: ", path)
-        if path.find("/fileUploadScript.php") != -1:
-            content_len = int(s.headers.get('Content-Length'))
-            post_body = s.rfile.read(content_len)
-            param_line = post_body.decode()
-            print("Body: ", param_line)
+        if path.find("/uploadFile") != -1:
+            #content_len = int(s.headers.get('Content-Length'))
+            #post_body = s.rfile.read(content_len)
+            #param_line = post_body.decode()
+            #print("Body: ", param_line)
 
-            s.send_response(200)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            s.wfile.write(bytes('<form action="fileUploadScript.php" method="post" enctype="multipart/form-data">', 'utf-8'))
+            s.wfile.write(bytes('<form action="uploadFile" method="post" enctype="multipart/form-data">', 'utf-8'))
             s.wfile.write(bytes('<html><body><h2>Weldability Checker:</h2>', "utf-8"))
             s.wfile.write(bytes('<br> Thank you for using our weldability checker!'))
 
-            s.wfile.write(bytes('<br>Upload your .prt file containing the welding lines<br><input type="file" name="the_file" id="fileToUpload">', "utf-8"))
+            s.wfile.write(bytes('<br>Upload your .prt file containing the welding lines<br><input type="file" id="myFile" name="myFile">', "utf-8"))
             s.wfile.write(bytes('<br><input type="submit" name="Upload file" value="Upload file">', "utf-8"))
 
             s.wfile.write(bytes('<br><br>Selct type of nozzle:<br><select name="nozzle" id="nozzle"><option value="RECESSED">Recessed</option><option value="FLUSH">Flush</option><option value="PROTRUDING">Protruding</option><option value="ADJUSTABLE">Adjustable</option><option value="CUSTOM">Custom</option></select>', "utf-8"))
@@ -104,13 +115,35 @@ def do_POST(s):
             s.wfile.write(bytes('</body></html>', "utf-8"))
 
 
+    def deal_post_data(s):
+        ctype, pdict = cgi.parse_header(self.headers['Content-Type'])
+        pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+        pdict['CONTENT-LENGTH'] = int(self.headers['Content-Length'])
+        if ctype == 'multipart/form-data':
+            form = cgi.FieldStorage( fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'], })
+            print (type(form))
+            try:
+                if isinstance(form["file"], list):
+                    for record in form["file"]:
+                        open("./uploads", "wb").write(record.file.read())
+                else:
+                    open("./uploads"%form["file"].filename, "wb").write(form["file"].file.read())
+            except IOError:
+                    return (False, "Can't create file to write, do you have permission to write?")
+        return (True, "Files uploaded")
+
         
 
 if __name__ == '__main__':
-    server_class = HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
+    #httpd = socketserver.TCPServer((HOST_NAME, PORT_NUMBER), MyHandler)
+    #print("serving at port", PORT_NUMBER)
+    #server_class = HTTPServer
+    #httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+
+    with socketserver.TCPServer((HOST_NAME, PORT_NUMBER), MyHandler) as httpd:
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        print("serving at port", PORT_NUMBER)
+        httpd.server_close()
